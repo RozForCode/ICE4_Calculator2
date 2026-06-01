6d83272dcdb10cce281155e72e4738411af9468a
Navrose Singh Johal <johnavro@sheridancollege.ca>
2026-06-01 13:29:27 -0400

    Initial setup

---
+++ ICE4_Calculator2/src/main/java/ca/sheridancollege/ice4_calculator2/ICE4_Calculator2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ca.sheridancollege.ice4_calculator2;
+
+/**
+ *
+ * @author navro
+ */
+public class ICE4_Calculator2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
