2c7f1e87702a3e0d694d7b30ecb25f98ea2a26e7
Navrose Singh Johal <johnavro@sheridancollege.ca>
2026-06-01 13:33:10 -0400

    Adding calculation functions to the main class file

--- ICE4_Calculator2/src/main/java/ca/sheridancollege/ice4_calculator2/ICE4_Calculator2.java
+++ ICE4_Calculator2/src/main/java/ca/sheridancollege/ice4_calculator2/ICE4_Calculator2.java
@@ -13,4 +13,25 @@ public class ICE4_Calculator2 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero.");
+        }
+        return (double) a / b;
+    }
+    public double power(double base, double exponent) {
+        return Math.pow(base, exponent);
+    }
 }
